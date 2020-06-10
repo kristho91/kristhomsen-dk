d90eeef50e53822c3b50b198f9ace25a04471f99
Kris Thomsen <mail@kristhomsen.dk>
2020-06-10 21:37:46 +0200

    Lots of cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,18 @@
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+module.exports = function(config) {
+  config.addPassthroughCopy('src/assets');
+  config.addLayoutAlias('page', 'layouts/page.njk');
 
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
-
-    eleventyConfig.addPassthroughCopy('src/assets');
-
-    return {
-        dir: {
-            data: '_data',
-            input: 'src',
-            includes: '_includes',
-            output: '_site'
-        },
-        templateFormats: [
-            'md',
-            'njk'
-        ],
-        htmlTemplateEngine: 'njk',
-        passthroughFileCopy: true
-    };
+  return {
+    dir: {
+      data: '_data',
+      includes: '_includes',
+      input: 'src',
+      output: 'dist'
+    },
+    templateFormats: [
+      'md',
+      'njk'
+    ],
+    passthroughFileCopy: true
+  };
 };
