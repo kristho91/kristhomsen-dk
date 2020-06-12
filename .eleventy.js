77aa53d429de684e752627a1284f87660bde0c0b
Kris Thomsen <mail@kristhomsen.dk>
2020-06-12 22:13:48 +0200

    Added styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function(config) {
-  config.addPassthroughCopy('src/assets');
   config.addLayoutAlias('page', 'layouts/page.njk');
+  config.addPassthroughCopy('src/assets');
+
+  config.addPassthroughCopy({
+    './node_modules/normalize.css/normalize.css': './assets/normalize.css'
+  });
+  config.addPassthroughCopy({
+    './node_modules/sakura.css/css/sakura-vader.css': './assets/sakura-vader.css'
+  });
 
   return {
     dir: {
