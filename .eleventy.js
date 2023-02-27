5c041ee2410aa759f25b33f77cb469541639b17c
Kris Thomsen <mail@kristhomsen.dk>
2023-02-27 22:07:36 +0100

    Updated 11.ty to 2.0

--- .eleventy.js
+++ .eleventy.js
@@ -7,9 +7,7 @@ module.exports = function(config) {
   config.addPassthroughCopy('src/assets');
 
   config.addPassthroughCopy({
-    './node_modules/normalize.css/normalize.css': './assets/normalize.css'
-  });
-  config.addPassthroughCopy({
+    './node_modules/normalize.css/normalize.css': './assets/normalize.css',
     './node_modules/sakura.css/css/sakura-vader.css': './assets/sakura-vader.css'
   });
 
